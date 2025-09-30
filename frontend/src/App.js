4b9dae8402850fa3715cdb4e7c8c21f6adf76f99
Facundo Olmedo <ofacundo785@gmail.com>
2025-09-30 15:12:59 -0300

    Plantilla mínima Django y React para iniciar desarrollo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Gestor de Documentación Escolar</h1>
+      <p>¡Bienvenido!</p>
+    </div>
+  );
+}
+
+export default App;
